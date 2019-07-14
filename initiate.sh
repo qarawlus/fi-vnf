f0db93c5a8bbc9fe847b9711212b99b4c567ea42
Haydar Qarawlus <haydar.qarawlus@gmail.com>
2019-07-14 13:40:34 +0200

    Corrected initiate script

--- initiate.sh
+++ initiate.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 brctl addbr br0
 ifconfig sf-eth0 0.0.0.0 promisc
 ifconfig sf-eth1 0.0.0.0 promisc
@@ -8,11 +7,4 @@ brctl addif br0 sf-eth0
 brctl addif br0 sf-eth1
 ifconfig br0 10.0.0.3
 
-iptables -m physdev --physdev-in sf-eth0 -A FORWARD -j NFQUEUE --queue-num 1
-
-
-#iptables -t raw -A PREROUTING -p tcp --destination-port 9001 -j NFQUEUE --queue-num 1
-
-# iptables -t raw -A PREROUTING -p tcp --destination-port 9001 -j NFQUEUE --queue-num 1
-
-# iptables -t nat -A POSTROUTING -o eth0-j MASQUERADE
+iptables -m physdev --physdev-in sf-eth0 -A FORWARD -p tcp --dport 25 -j NFQUEUE --queue-num 1
